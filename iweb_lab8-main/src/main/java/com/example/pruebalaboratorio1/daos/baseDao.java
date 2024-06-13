4abc0a3bfffee284130b2ea25cdd70fa590b20ce
NiloRC <145509554+Nilorc@users.noreply.github.com>
2024-06-12 21:57:54 -0500

    Add files via upload

---
+++ iweb_lab8-main/src/main/java/com/example/pruebalaboratorio1/daos/baseDao.java
@@ -0,0 +1,25 @@
+package com.example.pruebalaboratorio1.daos;
+
+import com.example.pruebalaboratorio1.beans.pelicula;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class baseDao {
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        String user = "root";
+        String pass = "123456";
+        String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
+
+        return DriverManager.getConnection(url, user, pass);
+
+    }
+}
